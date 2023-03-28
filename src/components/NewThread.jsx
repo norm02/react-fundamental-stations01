ee5b59d23d383cb78b4cad4edda20910c9a619d3
norm02 <richa0920@gmail.com>
2023-03-28 17:00:49 +0900

    station3

--- src/components/NewThread.jsx
+++ src/components/NewThread.jsx
@@ -1,58 +1,50 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 export function NewThread() {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    try {
-      const response = await fetch(
-        "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com//threads/",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ title, description }),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Failed to create thread");
+    fetch(
+      "https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads",
+      {
+        mode: "no-cors",
+        method: "POST",
+        headers: {
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Methods": "POST,GET,PUT,DELETE",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+        body: JSON.stringify({ title: event.target.title.value }),
       }
-      navigate("/Home"); // スレッド一覧ページに戻る
-    } catch (error) {
-      console.error(error);
-    }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then(() => {
+        navigate("/");
+      });
   };
 
   return (
-    <div className="new-thread-container">
-      <header>
-        <h1>スレッド新規作成</h1>
-      </header>
-      <form onSubmit={handleSubmit}>
-        <label>
-          タイトル:
-          <input
-            type="text"
-            value={title}
-            onChange={(event) => setTitle(event.target.value)}
-            required
-          />
-        </label>
-        <label>
-          説明:
-          <textarea
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">スレッドを作成する</button>
+    <div className="sled-container">
+      <h1>新規投稿</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="title">
+            タイトル
+            <input type="text" className="text" name="title" />
+          </label>
+        </div>
+        <br />
+        <button type="submit" className="btn btn-primary">
+          投稿
+        </button>
       </form>
+      <div className="underform-left">
+        <Link to={"/"}>Topに戻る</Link>
+      </div>
     </div>
   );
 }

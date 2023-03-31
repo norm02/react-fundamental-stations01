1f678c030f48433489b79c7c8fbb1dde88a1dcdb
norm02 <richa0920@gmail.com>
2023-03-31 21:35:32 +0900

    station3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/thread/new" element={<NewThread />} />
-          <Route path="/Thread" element={<Thread />} />
+          <Route path="/thread/:thread_id" element={<Thread />} />
         </Routes>
       </BrowserRouter>
     </div>
